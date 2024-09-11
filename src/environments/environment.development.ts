1fde2d71d2f680d615d4e7cb9a25b7db4e93f226
gianTrm <82704255+Gianbho@users.noreply.github.com>
2024-09-11 15:03:46 +0200

    cache cleared

--- src/environments/environment.development.ts
+++
@@ -1,4 +0,0 @@
-export const environment = {
-    production: false,
-    apiUrl: 'https://private-anon-a4f3ef963d-pizzaapp.apiary-mock.com',
-};
--- src/environments/environment.ts
+++
@@ -1,4 +0,0 @@
-export const environment = {
-    production: true,
-    apiUrl: 'https://private-anon-a4f3ef963d-pizzaapp.apiary-mock.com',
-};
